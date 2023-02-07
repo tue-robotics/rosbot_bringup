70b659e6a1e92c069342fa27b0a29fa9d51a63a0
Koen de Vos <k.d.vos@tue.nl>
2023-02-07 16:34:02 +0100

    clang-format

--- src/laser_transformer.cpp
+++ src/laser_transformer.cpp
@@ -3,11 +3,11 @@
 
 #include <math.h>
 
-#define ANGLE_MAX 3*M_PI_4
+#define ANGLE_MAX 3 * M_PI_4
 
 class LaserTransformer
 {
-    public:
+public:
     LaserTransformer()
     {
         ros::NodeHandle nh;
@@ -15,8 +15,8 @@ class LaserTransformer
         laser_pub = nh.advertise<sensor_msgs::LaserScan>("/transformed_scan", 1);
     }
 
-    private:
-    void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+private:
+    void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
     {
         sensor_msgs::LaserScan transformed_msg;
 
@@ -27,11 +27,11 @@ class LaserTransformer
         transformed_msg.angle_min = -ANGLE_MAX;
         transformed_msg.angle_increment = msg->angle_increment;
 
-        uint i_a_min = std::round((2*M_PI - ANGLE_MAX) / msg->angle_increment);
+        uint i_a_min = std::round((2 * M_PI - ANGLE_MAX) / msg->angle_increment);
         uint i_a_max = std::round(ANGLE_MAX / msg->angle_increment);
         uint n = msg->ranges.size();
-        uint i_0 = n-i_a_min;
-        uint n_new = std::round(2*ANGLE_MAX/msg->angle_increment) + 1;
+        uint i_0 = n - i_a_min;
+        uint n_new = std::round(2 * ANGLE_MAX / msg->angle_increment) + 1;
 
         ROS_INFO("index new max angle %i", i_a_max);
         ROS_INFO("index new min angle %i", i_a_min);
@@ -39,15 +39,15 @@ class LaserTransformer
         ROS_INFO("new_size %i", n_new);
 
         transformed_msg.ranges.resize(n_new);
-        for (int i = 0; i<i_0; i++)
+        for (int i = 0; i < i_0; i++)
         {
-            //ROS_INFO("i_new: %i i_old: %i", i, i+i_a_min);
-            transformed_msg.ranges[i] = msg->ranges[i+i_a_min];
+            // ROS_INFO("i_new: %i i_old: %i", i, i+i_a_min);
+            transformed_msg.ranges[i] = msg->ranges[i + i_a_min];
         }
-        for (int i = i_0; i<n_new; i++) // i=0; i<(=)i_a_max?
+        for (int i = i_0; i < n_new; i++) // i=0; i<(=)i_a_max?
         {
-            //ROS_INFO("i_new: %i i_old: %i", i, i-i_0);
-            transformed_msg.ranges[i] = msg->ranges[i-i_0]; // i?
+            // ROS_INFO("i_new: %i i_old: %i", i, i-i_0);
+            transformed_msg.ranges[i] = msg->ranges[i - i_0]; // i?
         }
 
         laser_pub.publish(transformed_msg);
@@ -63,13 +63,13 @@ class LaserTransformer
  */
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "laser_transformer");
+    ros::init(argc, argv, "laser_transformer");
 
-  ROS_INFO("started transformer node");
-  LaserTransformer laserTransformer = LaserTransformer();
+    ROS_INFO("started transformer node");
+    LaserTransformer laserTransformer = LaserTransformer();
 
-  ROS_INFO("spinning");
-  ros::spin();
+    ROS_INFO("spinning");
+    ros::spin();
 
-  return 0;
+    return 0;
 }
